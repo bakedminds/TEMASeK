e7c6b392b62cd88008fdc80ef39c18b4fd9da542
TEMASeK CryptoCoin <41658824+TEMASeKCC@users.noreply.github.com>
2018-07-30 20:47:01 +0800

    Update WalletConfig.h

--- src/zedwallet/WalletConfig.h
+++ src/zedwallet/WalletConfig.h
@@ -35,7 +35,7 @@ namespace WalletConfig
     const std::string coinName = std::string(CryptoNote::CRYPTONOTE_NAME);
 
     /* Where can your users contact you for support? E.g. discord */
-    const std::string contactLink = "http://temasek.discord";
+    const std::string contactLink = "https://discord.gg/E9GwCE6";
 
 
     /* The number of decimals your coin has */
